9906b6e06543e3246a6cb898c907b72eb0c1a9bf
praveenng <praveeng271@gmail.com>
2015-04-26 15:37:13 +0530

    updating php script printing just the headlines corresponding to the inflection points dates

--- backend/getNews.php
+++ backend/getNews.php
@@ -1,11 +1,11 @@
 <?php
-set_time_limit(200);//to increase the maximum execution time as this script takes more than 30 secs to execute(default max execution time is 30secs).
-$company = $argv[1]; //1st command line argument(company name)
-//$company = 'intel';
+set_time_limit(300);//to increase the maximum execution time as this script takes more than 30 secs to execute(default max execution time is 30secs).
+//$company = $argv[1]; //1st command line argument(company name)
+$company = 'intel';
 $output = $company."_output.txt";
 //to increase the maximum execution time as this script takes more than 30 secs to execute(default max execution time is 30secs).
 $datesContent = file_get_contents('http://localhost/getNews/'.$company.'.txt');
-//input file is the file with the inflection points dates 
+//new.txt is the file with the inflection points dates 
 $dates = explode("\n", $datesContent);
 //inflection points dates are in this array
 //removing the white space at the end for every date
@@ -13,20 +13,31 @@ for($i=0;$i<count($dates);$i++ ){
 	$dates[$i]=substr($dates[$i], 0,8);
 }
 $curl = curl_init();
-$result = '';
+$headLines = "";
 foreach ($dates as $value) {
 	
 	curl_setopt_array($curl, array(
 	CURLOPT_RETURNTRANSFER => 1,
 	CURLOPT_URL => 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='.$company.'&begin_date='.$value.'&end_date='.$value.'&api-key=7a7e9e5fc8f068b4cf9457a1abe884e5%3A2%3A71729777',
 	));
-	$resp = $value.' '.curl_exec($curl);; //printing date and news together
-	$result = $result.$resp.PHP_EOL; //all the dates and news
-	$result = $result.PHP_EOL; //adding a next line character between every set of date and news
+	$json_string = curl_exec($curl);
+	$count_matches = preg_match_all("/main\":\"(.*?)\"/", $json_string, $output_array);  //regex to get only the headlines
+	if($count_matches==0)
+	{
+		$headLines = $headLines."NO NEWS";
+	}
+	else{
+		foreach ($output_array[1] as $key => $value) {
+			
+			$headLines = $headLines.$value."~";
+		}
+	}
+		$headLines = $headLines.PHP_EOL.PHP_EOL;
+	
 }
-echo $result;
+//echo $result;
 $file = fopen($output, "w");
-fwrite($file,$result);
+fwrite($file,$headLines);
 fclose($file);
 curl_close($curl);
 ?>
