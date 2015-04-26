a7ac76af856b0b5be89d9ecfccc24ef80376c232
nitinbhyri <nitinbhyri@gmail.com>
2015-04-26 20:48:20 +0530

    corrected code

--- backend/stock.R
+++ backend/stock.R
@@ -29,22 +29,40 @@ scores <- sapply(k:(length(v2)-k), FUN=function(i){
 })
 inflections <- (k:(length(v2)-k))[scores>=cutoff]
 coname<-unlist(strsplit(args[1], "[.]"))
+newsconame<-gsub("Normalized","",coname)
+newsconame<-paste(newsconame[1],"_news",sep="")
 coname<-paste(coname[1],"html",sep=".")
 ms<- transform(ms,Date = as.character(Date))
-setwd('/var/www/frontend/SE/')
+
 ms$tr<-FALSE
 ms$tr[inflections]<-TRUE
+news<-read.csv(newsconame,sep="\n",header=FALSE)
+names(news)<-c("one")
+#print(news)
+#print(names(news))
 ms$news<-"Nothing"
-ms$news[inflections]<-"This is a inflection point"
+#print(news[1])
+ms$news[inflections]<-as.vector(news$one)
+#print(as.vector(news$one[3]))
+print(length(news$one))
+print(length(ms$news[inflections]))
+setwd('/var/www/frontend/SE/')
 m1<-mPlot(x="Date",y="Value",type="Line",data=ms)
 m1$set(pointSize = 0, lineWidth = 1)
 m1$set(hoverCallback = "#! function(index, options, content){
   var row = options.data[index]
+  var res=row.news
+  res=res.split('~')
+  total=res.length
+  news=''
+  for(i=0;i<total-1;i++){
+  	news=news+(i+1)+')'+res[i]+'</br>'
+  }
       if(row.tr==true)
       {  
       return '<b>' + row.Date + '</b>' + '<br/>' +
       'Value: ' + row.Value + '<br/>'+
-        'news:'+'<br/>'+row.news
+        'news:'+'<br/>'+news
       }
       else{
         return '<b>' + row.Date + '</b>' + '<br/>' +
